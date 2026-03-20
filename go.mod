2e9c5a952c235f934f5504d846d5ab75af2ae80c
never00rei <jwashington104.jw@gmail.com>
2026-03-20 19:51:42 +0000

    initial setup

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/never00rei/Follower
 
-go 1.21.3
+go 1.25.7
---
+++ internal/config/config.go
@@ -0,0 +1,11 @@
+package config
+
+const (
+	ConfigFile                string = "credentials"
+	ConfigFolder              string = "follower"
+	EnvSessionBearerToken     string = "FOLLOWER_SESSION_TOKEN"
+	EnvSessionTokenExpiryTime string = "FOLLOWER_SESSION_EXPIRY"
+	EnvSessionTenant          string = "FOLLOWER_TENANT"
+	EnvSessionApiUrl          string = "AUTH0_SESSION_API_URL"
+	AtlassianApiVersion       string = "2"
+)
